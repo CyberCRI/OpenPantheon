f75562fdb99e14d1293c53c2202eaf3e4a168966
François Travais <francois.travais@cri-paris.org>
2021-09-14 14:58:37 +0200

    Using fastapi-mail rather than emails

---
+++ backend/app/app/security.py
@@ -0,0 +1,45 @@
+# OpenPantheon: the pantheon for Education
+# Copyright (C) 2021 CRI
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+from datetime import datetime, timedelta
+from typing import Optional
+
+from jose import jwt
+
+from app.core.config import settings
+
+
+def generate_password_reset_token(email: str) -> str:
+    delta = timedelta(hours=settings.EMAIL_RESET_TOKEN_EXPIRE_HOURS)
+    now = datetime.utcnow()
+    expires = now + delta
+    exp = expires.timestamp()
+    return jwt.encode(
+        {
+            "exp": exp,
+            "nbf": now,
+            "sub": email
+        },
+        settings.SECRET_KEY,
+        algorithm="HS256",
+    )
+
+
+def verify_password_reset_token(token: str) -> Optional[str]:
+    try:
+        decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+        return decoded_token["email"]
+    except jwt.JWTError:
+        return None
