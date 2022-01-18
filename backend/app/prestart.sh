edfe12873d7bf6cbf32fc1d74f981ee18da7169e
François Travais <francois.travais@cri-paris.org>
2022-01-18 17:36:52 +0100

    Revert "Add a snippet to debug the backend envs"
    This reverts commit aab470d477eb688ea4860b9b13e779f84fbdbbef.

--- backend/app/prestart.sh
+++ backend/app/prestart.sh
@@ -18,11 +18,6 @@
 set -e
 set -x
 
-# Debug environment variables
-if [ "x$DEBUG_ENV" == "xTrue" ]; then
-  env | sort
-fi
-
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Let the DB start
